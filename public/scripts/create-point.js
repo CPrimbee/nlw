f813e915d9f09b4fbbcba815aa39d4b2dd02c43b
CPrimbee <cprimbee@gmail.com>
2020-06-16 14:26:30 -0300

    Curso finalizado

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,91 +1,86 @@
 function populateUFs() {
-    const stateSelect = document.querySelector("select[name=uf]")
+  const stateSelect = document.querySelector("select[name=uf]");
 
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-        .then(res => res.json())
-        .then(states => {
-
-            for (const state of states) {
-                stateSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-            }
-
-        })
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((res) => res.json())
+    .then((states) => {
+      for (const state of states) {
+        stateSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+      }
+    });
 }
 
-populateUFs()
+populateUFs();
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateInput = document.querySelector("input[name=state]")
+  const citySelect = document.querySelector("select[name=city]");
+  const stateInput = document.querySelector("input[name=state]");
 
-    const stateValue = event.target.value
+  const stateValue = event.target.value;
 
-    const indexOfSelectedState = event.target.selectedIndex
+  const indexOfSelectedState = event.target.selectedIndex;
 
-    stateInput.value = event.target.options[indexOfSelectedState].text
+  stateInput.value = event.target.options[indexOfSelectedState].text;
 
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateValue}/municipios`
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateValue}/municipios`;
 
-    citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
-    citySelect.disabled = true
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+  citySelect.disabled = true;
 
-    fetch(url)
-        .then(res => res.json())
-        .then(cities => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((cities) => {
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
+      }
 
-            for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
-            }
-
-            citySelect.disabled = false
-        })
+      citySelect.disabled = false;
+    });
 }
 
-document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
 
 // Itens de coleta
 // pegar todos os li's
-const itemsToCollect = document.querySelectorAll(".items-grid li")
+const itemsToCollect = document.querySelectorAll(".items-grid li");
 
 for (const item of itemsToCollect) {
-    item.addEventListener("click", handleSelectedItem)
+  item.addEventListener("click", handleSelectedItem);
 }
 
-const collectedItems = document.querySelector("input[name=items]")
+const collectedItems = document.querySelector("input[name=items]");
 
-let selectedItems = []
+let selectedItems = [];
 
 function handleSelectedItem(event) {
-    const itemLi = event.target
+  const itemLi = event.target;
 
-    // adicionar ou remover uma classe com javascript
-    itemLi.classList.toggle("selected")
+  // adicionar ou remover uma classe com javascript
+  itemLi.classList.toggle("selected");
 
-    const itemId = itemLi.dataset.id
+  const itemId = itemLi.dataset.id;
 
-    // verificar se existem items selecionados, se sim
-    // pegar os items selecionados
-    const alreadySelected = selectedItems.findIndex(item => {
-        const itemFound = item == itemId
-        return itemFound
-    })
+  // verificar se existem items selecionados, se sim
+  // pegar os items selecionados
+  const alreadySelected = selectedItems.findIndex((item) => {
+    const itemFound = item == itemId;
+    return itemFound;
+  });
 
-    // se já estiver selecionado, tirar da seleção
-    if (alreadySelected >= 0) {
-        // tirar da seleção
-        const filteredItems = selectedItems.filter(item => {
-            const itemIsDifferent = item != itemId
-            return itemIsDifferent
-        })
+  // se já estiver selecionado, tirar da seleção
+  if (alreadySelected >= 0) {
+    // tirar da seleção
+    const filteredItems = selectedItems.filter((item) => {
+      const itemIsDifferent = item != itemId;
+      return itemIsDifferent;
+    });
 
-        selectedItems = filteredItems
-    } else {
-        // se não estiver selecionado, adicionar à seleção
-        selectedItems.push(itemId)
-    }
+    selectedItems = filteredItems;
+  } else {
+    // se não estiver selecionado, adicionar à seleção
+    selectedItems.push(itemId);
+  }
 
-    // atualizar o campo escondido com os items selecionados
-    collectedItems.value = selectedItems
+  // atualizar o campo escondido com os items selecionados
+  collectedItems.value = selectedItems;
 }
